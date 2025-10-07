10355e485d61f92e612eebe9cbf712fee651e381
Ahsan Habib <157474063+ahmhabib01@users.noreply.github.com>
2025-10-07 23:37:19 +0600

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let isResponseGenerating = false;
 
 // API configuration
 const API_KEY = "AIzaSyCIP2zrqg4k92cF_frPTU1syDd8j8fA5Vg"; // Your API key here
-const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
 
 // Load theme and chat data from local storage on page load
 const loadDataFromLocalstorage = () => {
